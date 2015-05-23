3b7c80c03e1ec5f052ca97f9be930d19297935e1
fourtwentytwo <fourtwentytwo@yandex.com>
2015-05-23 17:55:43 +0300

    3 hours for target timespan and 15 seconds for target spacing

--- core/src/main/java/org/bitcoinj/core/NetworkParameters.java
+++ core/src/main/java/org/bitcoinj/core/NetworkParameters.java
@@ -120,8 +120,8 @@ public abstract class NetworkParameters implements Serializable {
         return genesisBlock;
     }
 
-    public static final int TARGET_TIMESPAN = 14 * 24 * 60 * 60;  // 2 weeks per difficulty cycle, on average.
-    public static final int TARGET_SPACING = 10 * 60;  // 10 minutes per block.
+    public static final int TARGET_TIMESPAN = 3 * 60 * 60;  // 3 hours for difficulty retargeting window, on average.
+    public static final int TARGET_SPACING = 15;  // 10 seconds per block.
     public static final int INTERVAL = TARGET_TIMESPAN / TARGET_SPACING;
     
     /**
